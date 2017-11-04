e899628153dc8e42492a70e028e5714bb989b484
nehaha <n.satapathy02@gmail.com>
2017-11-04 00:58:33 -0400

    transactions

---
+++ gs-rest-service-master/complete/src/main/java/Transactions/Transactions.java
@@ -0,0 +1,14 @@
+package Transactions;
+
+import TableObject.Person;
+
+public class Transactions {
+
+	public static void decrement(Person person, double sub) {
+		person.decrBalance(sub);
+	}
+
+	public static void increment(Person person, double add) {
+		person.incrBalance(add);
+	}
+}
