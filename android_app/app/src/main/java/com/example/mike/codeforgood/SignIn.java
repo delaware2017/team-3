8afbf2c8e3711e925b751f790cab92deb7cfbb3f
nehaha <n.satapathy02@gmail.com>
2017-11-04 10:10:34 -0400

    main page

--- android_app/app/src/main/java/com/example/mike/codeforgood/SignIn.java
+++ android_app/app/src/main/java/com/example/mike/codeforgood/SignIn.java
@@ -29,17 +29,7 @@ public class SignIn extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 // Checks username and password with DB
-                if (!username.getText().toString().trim().equals("") ||
-                        !password.getText().toString().trim().equals("")) {
-                    Intent phone = new Intent(SignIn.this, leftscroller.class);
-                    phone.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
-                    startActivity(phone);
-                    finish();
-                }
-                else {
-                    Toast.makeText(getApplicationContext(), "Please enter username and " +
-                            "password", Toast.LENGTH_SHORT).show();
-                }
+                
             }
         });
 
